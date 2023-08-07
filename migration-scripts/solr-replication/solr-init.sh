6cb9e6c95e7912ff56ce10f16cb8183a0563c33d
GBo <g.boulangeot@redspher.com>
2023-08-07 13:20:57 +0200

    safe commit

--- migration-scripts/solr-replication/solr-init.sh
+++ migration-scripts/solr-replication/solr-init.sh
@@ -3,14 +3,15 @@
 # Wait for Solr to start
 until $(curl --output /dev/null --silent --head --fail http://localhost:8983/solr/admin/ping); do
     printf '.'
-    sleep 5
+    sleep 15
 done
 
 # Load schema (This is a placeholder, you might need to adjust based on how you want to use the schema.json with Solr's Config API)
-# curl "http://localhost:8983/solr/mycore/config" -d '@/opt/solr/server/solr/mycores/mycore/conf/schema.json'
+# curl "http://localhost:8983/solr/address-book/config" -d '@/opt/solr/server/solr/mycores/address-book/conf/schema.json'
 
 # Post data to Solr
-/opt/solr/bin/post -c mycore /opt/solr/server/solr/mycores/mycore/data/data.json
+/opt/solr/bin/post -c address-book /opt/solr/server/solr/mycores/address-book/data/data.json
+
 
 # Start Solr in the foreground
 exec solr-foreground
